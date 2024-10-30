857ae937c0593de2beed6375004e5855398d118e
frbird <frbird@gmail.com>
2024-10-29 19:17:48 -0500

    Fixed tests

--- test_unittest.py
+++ test_unittest.py
@@ -1,62 +1,69 @@
 import pytest
 import os
-import sys
 import yaml
 import requests
 from unittest.mock import patch, mock_open
 from tappecue_monitor import load_vars, req
 
-# Test load_vars function
-def test_load_vars_success():
-    mock_yaml_content = """
-    tappecue_user: "user"
-    tappecue_password: "password"
-    tappecue_api_url: "http://example.com"
-    logging_level: "INFO"
-    check_probe_delay: 30
-    no_session_delay: 60
-    """
-    with patch("builtins.open", mock_open(read_data=mock_yaml_content)):
-        config = load_vars("config.yaml")
-        assert config["tappecue_user"] == "user"
-        assert config["tappecue_password"] == "password"
-        assert config["tappecue_api_url"] == "http://example.com"
-        assert config["logging_level"] == "INFO"
-        assert config["check_probe_delay"] == 30
-        assert config["no_session_delay"] == 60
-
-def test_load_vars_file_not_found():
+# Check to be sure FileNotFoundError is raised when the file is not found.
+def test_load_vars_file_not_found(caplog):
     with patch("builtins.open", side_effect=FileNotFoundError):
-        with pytest.raises(SystemExit):
+        with pytest.raises(FileNotFoundError):
             load_vars("config.yaml")
+        assert "Error reading config file" in caplog.text
 
-def test_load_vars_yaml_error():
-    with patch("builtins.open", side_effect=yaml.YAMLError):
-        with pytest.raises(SystemExit):
+def test_load_vars_yaml_error(caplog):
+    invalid_yaml_content = """
+    tappecue_user "user"
+    tappecue_api_url: 
+    "http://example.com"
+    """
+    with patch("builtins.open", mock_open(read_data=invalid_yaml_content)):
+        with pytest.raises(yaml.YAMLError):
             load_vars("config.yaml")
+        assert "Error parsing YAML file" in caplog.text
 
 def test_load_environment_vars():
     mock_yaml_content = """
     tappecue_user: "yaml_user"
     tappecue_password: "yaml_password"
     tappecue_api_url: "yaml-http://example.com"
-    logging_level: "yaml-INFO"
+    log_level: "yaml-INFO"
     check_probe_delay: "yaml-30"
     no_session_delay: "yaml-60"
     """
     with patch("builtins.open", mock_open(read_data=mock_yaml_content)):
-        config = load_vars("config.yaml")
         os.environ["TAPPECUE_USER"] = "user"
         os.environ["TAPPECUE_PASSWORD"] = "password"
         os.environ["TAPPECUE_API_URL"] = "http://example.com"
         os.environ["LOGGING_LEVEL"] = "INFO"
         os.environ["CHECK_PROBE_DELAY"] = "30"
         os.environ["NO_SESSION_DELAY"] = "60"
-        assert config["tappecue_user"] == "user"
-        assert config["tappecue_password"] == "password"
-        assert config["tappecue_api_url"] == "http://example.com"
-        assert config["logging_level"] == "INFO"
-        assert config["check_probe_delay"] == 30
+        os.environ["LOG_LEVEL"] = "INFO"
+        config = load_vars("config.yaml")
+        assert config["user"] == "user"
+        assert config["password"] == "password"
+        assert config["base_url"] == "http://example.com"
+        assert config["log_level"] == "INFO"
+        assert config["check_delay"] == 30
+        assert config["no_session_delay"] == 60
+
+def test_load_config_file_vars():
+    mock_yaml_content = """
+    tappecue_user: "yaml_user"
+    tappecue_password: "yaml_password"
+    tappecue_api_url: "yaml-http://example.com"
+    log_level: "yaml-INFO"
+    check_probe_delay: "30"
+    no_session_delay: "60"
+    """
+    with patch("builtins.open", mock_open(read_data=mock_yaml_content)):
+        config = load_vars("config.yaml")
+        assert config["user"] == "yaml_user"
+        assert config["password"] == "yaml_password"
+        assert config["base_url"] == "yaml-http://example.com"
+        assert config["log_level"] == "yaml-INFO"
+        assert config["check_delay"] == 30
         assert config["no_session_delay"] == 60
 
 # Test req function
